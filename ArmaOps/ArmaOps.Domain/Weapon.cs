37bc78014dc31d2b85fd503f374d03bcb3c7edf0
mervaka <mervaka>
2020-04-17 00:36:11 +0100

    Weapon class, first commit

---
+++ ArmaOps/ArmaOps.Domain/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmaOps.Domain
+{
+    public class Weapon
+    {
+        public string Name { get; }
+        public int MinElevation { get; }
+        public int MaxElevation { get; }
+        public IEnumerable<int> ChargeVelocity { get; }
+        public bool AllowDirectFire { get; }
+
+        public Weapon (string name, int minElevation, int maxElevation, IEnumerable<int> chargeVelocity, bool allowDirectFire)
+        {
+            Name = name;
+            MinElevation = minElevation;
+            MaxElevation = maxElevation;
+            ChargeVelocity = chargeVelocity;
+            AllowDirectFire = allowDirectFire;
+        }
+    }
+}
