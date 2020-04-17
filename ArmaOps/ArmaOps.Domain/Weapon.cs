f48e7510e8b85a95b6183fc54a2761687a8326ee
mervaka <mervaka>
2020-04-17 23:00:19 +0100

    Weapon code and most tests

--- ArmaOps/ArmaOps.Domain/Weapon.cs
+++ ArmaOps/ArmaOps.Domain/Weapon.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ArmaOps.Domain
@@ -10,15 +11,82 @@ namespace ArmaOps.Domain
         public int MinElevation { get; }
         public int MaxElevation { get; }
         public IEnumerable<int> ChargeVelocity { get; }
-        public bool AllowDirectFire { get; }
 
-        public Weapon (string name, int minElevation, int maxElevation, IEnumerable<int> chargeVelocity, bool allowDirectFire)
+        public Weapon (string name, int minElevation, int maxElevation, IEnumerable<int> chargeVelocity)
         {
+            if(string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Must specify a name!");
+            }
+            if(minElevation < -6400 || minElevation > 6400)
+            {
+                throw new ArgumentOutOfRangeException("Minimum elevation must be ±6400");
+            }
+            if (maxElevation < -6400 || maxElevation > 6400)
+            {
+                throw new ArgumentOutOfRangeException("Maximum elevation must be ±6400");
+            }
+            if (minElevation > maxElevation)
+            {
+                throw new ArgumentException("Minimum elevation cannot be greater than maximum elevation!");
+            }
+            int count = 0;
+            foreach(var cv in chargeVelocity)
+            {
+                ++count;
+                if(cv < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Velocity cannot be negative!");
+                }
+            }
+            if(count == 0)
+            {
+                throw new ArgumentException("Must specify at least one velocity!");
+            }
+
             Name = name;
             MinElevation = minElevation;
             MaxElevation = maxElevation;
             ChargeVelocity = chargeVelocity;
-            AllowDirectFire = allowDirectFire;
+        }
+
+        public Weapon EditName(string name)
+        {
+            return new Weapon(name, MinElevation, MaxElevation, ChargeVelocity);
+        }
+        public Weapon EditMinElevation(int minElevation)
+        {
+            return new Weapon(Name, minElevation, MaxElevation, ChargeVelocity);
+        }
+        public Weapon EditMaxElevation(int maxElevation)
+        {
+            return new Weapon(Name, MinElevation, maxElevation, ChargeVelocity);
+        }
+        public Weapon EditChargeVelocities(IEnumerable<int> chargeVelocity)
+        {
+            return new Weapon(Name, MinElevation, MaxElevation, chargeVelocity);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj != null)
+            {
+                var other = obj as Weapon;
+                if (other != null)
+                {
+                    return
+                        other.Name.Equals(this.Name)
+                        && other.MinElevation.Equals(this.MinElevation)
+                        && other.MaxElevation.Equals(this.MaxElevation)
+                        && other.ChargeVelocity.SequenceEqual(this.ChargeVelocity);
+                }
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name, MinElevation, MaxElevation, ChargeVelocity).GetHashCode();
         }
     }
 }
---
+++ ArmaOps/ArmaOps.Test/Domain/Weapon_Test.cs
@@ -0,0 +1,181 @@
+﻿using ArmaOps.Domain;
+using ArmaOps.Domain.Coordinates;
+using AutoFixture.NUnit3;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmaOps.Test.Domain
+{
+    [TestFixture]
+    public class Weapon_Test
+    {
+        [Test, AutoData]
+        public void EqualsTrueIfAllValuesMatch(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+            var sut = new Weapon(name, 1, 2, charges);
+            var sameValues = new Weapon(sut.Name, sut.MinElevation, sut.MaxElevation,sut.ChargeVelocity);
+
+            Assert.That(sameValues, Is.EqualTo(sut));
+            Assert.That(sameValues.GetHashCode(), Is.EqualTo(sut.GetHashCode()));
+        }
+
+        [Test, AutoData]
+        public void EqualsFalseIfNameMismatch(
+            string name,
+            string name2)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+            var sut = new Weapon(name, 1, 2, charges);
+            var other = new Weapon(name2, sut.MinElevation, sut.MaxElevation, sut.ChargeVelocity);
+
+            Assert.That(other, Is.Not.EqualTo(sut));
+            Assert.That(other.GetHashCode(), Is.Not.EqualTo(sut.GetHashCode()));
+        }
+
+        [Test, AutoData]
+        public void EqualsFalseIfMaxElevationMismatch(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+            var sut = new Weapon(name, 1, 2, charges);
+            var other = new Weapon(sut.Name, sut.MinElevation, 3, sut.ChargeVelocity);
+
+            Assert.That(other, Is.Not.EqualTo(sut));
+            Assert.That(other.GetHashCode(), Is.Not.EqualTo(sut.GetHashCode()));
+        }
+
+        [Test, AutoData]
+        public void EqualsFalseIfMinElevationMismatch(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+            var sut = new Weapon(name, 1, 2, charges);
+            var other = new Weapon(sut.Name, 0, sut.MaxElevation, sut.ChargeVelocity);
+
+            Assert.That(other, Is.Not.EqualTo(sut));
+            Assert.That(other.GetHashCode(), Is.Not.EqualTo(sut.GetHashCode()));
+        }
+
+
+        [Test, AutoData]
+        public void EqualsFalseIfChargeVelocityMismatch(
+            string name,
+            IEnumerable<int> chargeVelocity)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+            var sut = new Weapon(name, 1, 2, charges);
+            var other = new Weapon(sut.Name, sut.MinElevation, sut.MaxElevation, chargeVelocity);
+
+            Assert.That(other, Is.Not.EqualTo(sut));
+            Assert.That(other.GetHashCode(), Is.Not.EqualTo(sut.GetHashCode()));
+        }
+
+        [Test, AutoData]
+        public void EmptyNameThrowsArgumentException()
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentException>(() =>
+                new Weapon(string.Empty, -1, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MinElevationLessThanNeg6400ThrowsArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Weapon(name, -6401, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MinElevationGreaterThanThan6400ThrowsArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Weapon(name, 6401, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MaxElevationLessThanNeg6400ThrowsArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Weapon(name, 1, -6401, charges));
+        }
+
+        [Test, AutoData]
+        public void MaxElevationGreaterThanThan6400ThrowsArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Weapon(name, 1, 6401, charges));
+        }
+
+
+        [Test, AutoData]
+        public void EmptyChargeCollectionThrowsArgumentException(string name)
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new Weapon(name, -1, 1, new List<int>()));
+        }
+
+        [Test, AutoData]
+        public void ChargeLessThanZeroThrowsArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { -1 };
+
+            Assert.Throws<ArgumentOutOfRangeException>(() =>
+                new Weapon(name, -1, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MinElevationGreaterThanMaxThrowsArgumentException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.Throws<ArgumentException>(() =>
+                new Weapon(name, 2, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MinElevationNeg6400DoesntThrowArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.DoesNotThrow(() =>
+                new Weapon(name, -6400, 1, charges));
+        }
+
+        [Test, AutoData]
+        public void MinElevation6400DoesntThrowArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.DoesNotThrow(() =>
+                new Weapon(name, 6400, 6400, charges));
+        }
+
+        [Test, AutoData]
+        public void MaxElevationNeg6400DoesntThrowArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.DoesNotThrow(() =>
+                new Weapon(name, -6400, -6400, charges));
+        }
+
+        [Test, AutoData]
+        public void MaxElevation6400DoesntThrowArgumentOutOfRangeException(string name)
+        {
+            var charges = new List<int> { 0, 1, 2 };
+
+            Assert.DoesNotThrow(() =>
+                new Weapon(name, 1, 6400, charges));
+        }
+    }
+}
