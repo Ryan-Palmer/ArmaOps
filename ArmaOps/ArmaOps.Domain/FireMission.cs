080fdc8f6b9db81f0f23a63151d17fe4b46b6c76
mervaka <mervaka>
2020-04-19 13:59:51 +0100

    FireMission first commit

---
+++ ArmaOps/ArmaOps.Domain/FireMission.cs
@@ -0,0 +1,67 @@
+﻿using ArmaOps.Domain.Coordinates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmaOps.Domain
+{
+    public abstract class FireMission
+    {
+        public string Name { get; }
+        public Cartesian Target { get; }
+
+        public FireMission(string name, Cartesian target)
+        {
+            Name = name;
+            Target = target;
+        }
+
+        public FireMission(Cartesian target)
+        {
+            Name = Guid.NewGuid().ToString();
+            Target = target;
+        }
+    }
+
+    public class PolarFireMission : FireMission
+    {
+        public PolarFireMission(
+            ForwardObserver fo, Mils observedAzimuth,
+            Mils observedElevation, int observedDistanceMetres)
+            : base(fo.Name, GetCartesianTarget(fo, observedAzimuth, observedElevation, observedDistanceMetres))
+        {
+            
+        }
+
+        public PolarFireMission(
+            string name, ForwardObserver fo, Mils observedAzimuth,
+            Mils observedElevation, int observedDistanceMetres)
+            : base(name, GetCartesianTarget(fo, observedAzimuth, observedElevation, observedDistanceMetres))
+        {
+
+        }
+
+        static Cartesian GetCartesianTarget (ForwardObserver fo, Mils observedAzimuth,
+            Mils observedElevation, int observedDistanceMetres)
+        {
+            return new Polar(fo.Location,
+                observedAzimuth.Radians,
+                observedElevation.Radians,
+                observedDistanceMetres).ToCartesian();
+        }
+
+
+    }
+
+    public class GridFireMission : FireMission
+    {
+        public GridFireMission(Cartesian target) : base(target)
+        {
+            
+        }
+        public GridFireMission(string name, Cartesian target) : base(name, target)
+        {
+
+        }
+    }
+}
---
+++ ArmaOps/ArmaOps.Domain/Mils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArmaOps.Domain
+{
+    public class Mils
+    {
+        const double MultiplicandMilsToRadians = Math.PI / 3200.0;
+        const double MultiplicandRadiansToMils = 3200.0 / Math.PI;
+
+        public int Value { get; }
+        public double Radians => MultiplicandMilsToRadians * Value;
+
+        public Mils (int milliRadians)
+        {
+            Value = milliRadians;
+        }
+
+        public Mils(double radians)
+        {
+            Value = (int)Math.Round(radians * MultiplicandRadiansToMils);
+        }
+    }
+}
